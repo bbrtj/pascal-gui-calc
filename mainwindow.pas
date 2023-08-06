d6187d60bcd348ac237f8b2afd5c24aaff18866e
bbrtj <bbrtj.pro@gmail.com>
2023-08-06 18:11:38 +0200

    Initial: create the project

---
+++ mainwindow.pas
@@ -0,0 +1,30 @@
+unit MainWindow;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+	Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+	{ TMainForm }
+
+ TMainForm = class(TForm)
+		GroupBox1: TGroupBox;
+	private
+
+	public
+
+	end;
+
+var
+	MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
